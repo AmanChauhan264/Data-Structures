8d0d56e442f8e52f740e5711feb4efb336755a8d
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-05-28 14:08:30 +0530

    pushing pattren 3 code

---
+++ Patterns/Pattern3.cpp
@@ -0,0 +1,24 @@
+/*
+Pattern 3
+1
+12
+123
+1234
+*/
+
+#include<iostream>
+using namespace std;
+void Pattern3(int n){
+       for(int i = 1; i <= n ; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+       }
+}
+int main(){
+    int n;
+    cout<< "Enter the value of n : ";
+    cin >> n;
+    Pattern3(n);
+}
