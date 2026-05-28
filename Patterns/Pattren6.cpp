aa29d5cb8cc1a07b2f1f880d1f2f395d21014861
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-05-28 14:47:59 +0530

    pushing pattern 6 code

---
+++ Patterns/Pattren6.cpp
@@ -0,0 +1,26 @@
+/*
+Pattern 6
+12345
+1234
+123
+12
+1
+
+*/
+
+#include<iostream>
+using namespace std;
+void Pattern6(int n){
+       for(int i = n; i > 0; i--){
+        for( int j = 1; j <= i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    Pattern6(n);
+}
