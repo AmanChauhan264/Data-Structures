3b5b7da9f4fc67da1e6e547a8b36dfdb0b3500f5
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-26 10:40:17 +0530

    push array codes

---
+++ ProblemsOnArray/Basic.cpp
@@ -0,0 +1,9 @@
+//  1.   If we declare an array in side the main but did not initialize it's values then there will be random or garbage values on each indexes.
+
+// But if we declare an array globally then each index will contain zero as the element.
+
+//  2.  The maximum number of indexes a array can hold is 10^6 inside the main.
+
+// But 10^7 if it is declared globally.
+
+
