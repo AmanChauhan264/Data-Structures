bac98bc42a2ef8223b15d2d35f32af1d6be60f7d
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-30 07:22:49 +0530

    push add solution of missing number

--- ProblemsOnArray/MissingNumber.cpp
+++ ProblemsOnArray/MissingNumber.cpp
@@ -1,29 +1,54 @@
 // This solution is better than brute but not the best because there will be space complexity of O(n) as we used hash. Better solution is below this code.
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int Missing(vector<int> &arr, int n){
+//     int hash[n+1] = {0};
+//     for(int i = 0;i < n ; i++){
+//         hash[arr[i]] = 1;
+//     }
+//     for(int i = 1; i<n ; i++){
+//         if(hash[i]==0)
+//         return i;
+//     }
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter size of your array: ";
+//     cin>>n;
+//     vector<int> arr(n);
+//     cout<<"Enter elements: ";
+//     for(int i = 0; i<n-1 ; i++){
+//         cin>>arr[i];
+//     }
+//    int ans =  Missing(arr, n);
+//    cout<<"Missing number is: "<<ans;
+// }
+
+//------ Better solution using Sum --------->
+
+#include <bits/stdc++.h>
 using namespace std;
-
-int Missing(vector<int> &arr, int n){
-    int hash[n+1] = {0};
-    for(int i = 0;i < n ; i++){
-        hash[arr[i]] = 1;
+int Missing(vector<int> &arr, int n) {
+    int sum = n * (n + 1) / 2;
+    int arrSum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        arrSum += arr[i];
     }
-    for(int i = 1; i<n ; i++){
-        if(hash[i]==0)
-        return i;
-    }
+    return sum - arrSum;
 }
-
-int main(){
+int main() {
     int n;
-    cout<<"Enter size of your array: ";
-    cin>>n;
-    vector<int> arr(n);
-    cout<<"Enter elements: ";
-    for(int i = 0; i<n-1 ; i++){
-        cin>>arr[i];
+    cout << "Enter value of N: ";
+    cin >> n;
+    vector<int> arr(n - 1);
+    cout << "Enter " << n - 1 << " elements: ";
+    for (int i = 0; i < n - 1; i++) {
+        cin >> arr[i];
     }
-   int ans =  Missing(arr, n);
-   cout<<"Missing number is: "<<ans;
-   
+    int ans = Missing(arr, n);
+    cout << "Missing number is: " << ans;
+    return 0;
 }
