6256087ad1c307e5b085f740fd34bca9bd706865
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-04 18:42:48 +0530

    push hash map solution for 2sum problem

--- ProblemsOnArray-Medium/2Sum.cpp
+++ ProblemsOnArray-Medium/2Sum.cpp
@@ -1,32 +1,70 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// string sum2(int n, vector<int> arr, int target){
+//     int left = 0, right = n-1;
+//     sort(arr.begin(), arr.end());
+//     while(left<right){
+//         int sum = arr[left] + arr[right];
+//         if(sum == target){
+//             return "YES";
+//         }
+//         else if(sum < target) left++;
+//         else right--;
+//     }
+//     return "NO";
+// }
+
+// int main(){
+//     int n ;
+//     cout<<"Enter size of the array: ";
+//     cin>>n;
+//     vector<int> arr(n);
+//     cout<<"Enter elements of the array: ";
+//     for(int i = 0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     int target;
+//     cout<<"Enter target: ";
+//     cin>>target;
+//     string ans = sum2(n, arr, target);
+//     cout<<"Answer is "<<ans;
+// }
+
+// -----------Hash map solution -------------------------->
+
+#include <bits/stdc++.h>
 using namespace std;
 
-string sum2(int n, vector<int> arr, int target){
-    int left = 0, right = n-1;
-    sort(arr.begin(), arr.end());
-    while(left<right){
-        int sum = arr[left] + arr[right];
-        if(sum == target){
-            return "YES";
+pair<int, int> twoSum(vector<int>& arr, int target) {
+    unordered_map<int, int> mp;
+    for (int i = 0; i < arr.size(); i++) {
+        int complement = target - arr[i];
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
         }
-        else if(sum < target) left++;
-        else right--;
+        mp[arr[i]] = i;
     }
-    return "NO";
+    return {-1, -1};
 }
 
-int main(){
-    int n ;
-    cout<<"Enter size of the array: ";
-    cin>>n;
+int main() {
+    int n;
+    cout << "Enter size of the array: ";
+    cin >> n;
     vector<int> arr(n);
-    cout<<"Enter elements of the array: ";
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
+    cout << "Enter elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
     int target;
-    cout<<"Enter target: ";
-    cin>>target;
-    string ans = sum2(n, arr, target);
-    cout<<"Answer is "<<ans;
+    cout << "Enter target: ";
+    cin >> target;
+    pair<int, int> ans = twoSum(arr, target);
+    if (ans.first != -1) {
+        cout << "Indexes are: " << ans.first << " and " << ans.second;
+    } else {
+        cout << "No pair found";
+    }
+    return 0;
 }
