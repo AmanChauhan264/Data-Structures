b80c5d4ca33f8d4f1ce7b59a639a9e39025b57d7
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-09 11:57:17 +0530

    push optimal solution for longest cons seq

--- ProblemsOnArray-Medium/LongestConsecutiveSequence.cpp
+++ ProblemsOnArray-Medium/LongestConsecutiveSequence.cpp
@@ -1,33 +1,75 @@
 //  BRUTE FORCE SOLUTION
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// bool ls(vector<int> &arr, int num){
+//     int n = arr.size();
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] == num){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// void longestConsecutive(vector<int> &arr){
+//     int n = arr.size();
+//     int longest = 1;
+//     int count = 0;
+//     int x;
+//     for(int i = 0; i<n; i++){
+//         x = arr[i];
+//         count = 1;
+//         while(ls(arr, x+1) == true){
+//             x = x + 1;
+//             count = count+1;
+//         }
+//         if(longest<count) longest = count;
+//     }
+//     cout<<"Answer is: "<<longest;
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter size of the array: ";
+//     cin >> n;
+//     vector<int> arr(n);
+//     cout << "Enter elements of the array: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     longestConsecutive(arr);
+// }
+
+//  Optimal solution
+
+//  BRUTE FORCE SOLUTION
+
 #include<bits/stdc++.h>
 using namespace std;
 
-bool ls(vector<int> &arr, int num){
+int longestConsecutive(vector<int> &arr){
     int n = arr.size();
-    for(int i = 0; i < n; i++){
-        if(arr[i] == num){
-            return true;
-        }
-    }
-    return false;
-}
-
-void longestConsecutive(vector<int> &arr){
-    int n = arr.size();
+    if(n==0) return 0;
     int longest = 1;
-    int count = 0;
-    int x;
+    unordered_set<int> st;
     for(int i = 0; i<n; i++){
-        x = arr[i];
-        count = 1;
-        while(ls(arr, x+1) == true){
-            x = x + 1;
-            count = count+1;
-        }
-        if(longest<count) longest = count;
+        st.insert(arr[i]);
     }
-    cout<<"Answer is: "<<longest;
+    for(auto it: st){
+        if(st.find(it-1) == st.end()){
+            int cnt = 1;
+            int x = it;
+            while(st.find(x+1)!=st.end()){
+                x = x+1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+   
 }
 
 int main() {
@@ -39,5 +81,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    longestConsecutive(arr);
+    int ans = longestConsecutive(arr);
+    cout<<"Answer is: "<<ans;
 }
