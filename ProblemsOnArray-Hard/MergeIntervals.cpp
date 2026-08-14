10d6b8f63d76a1a7d0aec3fcf9e1a4b74df381c2
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-14 08:12:35 +0530

    push optimal sol for merge intervals

--- ProblemsOnArray-Hard/MergeIntervals.cpp
+++ ProblemsOnArray-Hard/MergeIntervals.cpp
@@ -1,3 +1,54 @@
+//  BRUTE FORCE SOLUTION
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> MergeIntervals(vector<vector<int>> &arr){
+//     int n = arr.size();
+//     sort(arr.begin(), arr.end());
+//     vector<vector<int>> ans;
+//     for(int i = 0; i<n; i++){
+//         int start = arr[i][0];
+//         int end = arr[i][1];
+//         if(!ans.empty() && end<=ans.back()[1]){
+//             continue;
+//         }
+//         for(int j = i+1; j<n; j++){
+//             if(arr[j][0] <= end){
+//                 end = max(end, arr[j][1]);
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         ans.push_back({start, end});
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     int n, m = 1;
+//     cout<<"Enter number of rows: ";
+//     cin>>n;
+//     vector<vector<int>> arr(n, vector<int>(2));
+//     cout<<"Enter elements: ";
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<2; j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     cout<<"Intervals are: ";
+//     vector<vector<int>> ans = MergeIntervals(arr);
+//     for(int i = 0; i < ans.size(); i++) {
+//         for(int j = 0; j < ans[i].size(); j++) {
+//             cout << ans[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//         OPTIMAL SOLUTION
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,20 +57,12 @@ vector<vector<int>> MergeIntervals(vector<vector<int>> &arr){
     sort(arr.begin(), arr.end());
     vector<vector<int>> ans;
     for(int i = 0; i<n; i++){
-        int start = arr[i][0];
-        int end = arr[i][1];
-        if(!ans.empty() && end<=ans.back()[1]){
-            continue;
+        if(ans.empty() || arr[i][0] > ans.back()[1]){
+            ans.push_back(arr[i]);
         }
-        for(int j = i+1; j<n; j++){
-            if(arr[j][0] <= end){
-                end = max(end, arr[j][1]);
-            }
-            else{
-                break;
-            }
+        else{
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
         }
-        ans.push_back({start, end});
     }
     return ans;
 }
