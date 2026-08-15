4339e52bffbc7419e96089f3e21252b09c9ec9b0
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-15 21:33:08 +0530

    push brute sol for ReversePairs

---
+++ ProblemsOnArray-Hard/ReversePairs.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ReversePairs(vector<int> &arr){
+    int n = arr.size();
+    int cnt = 0;
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n-1; j++){
+            if(arr[i] > 2*arr[j]) cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int n;
+    cout<<"Enter size of your array: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter elements: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    int ans = ReversePairs(arr);
+    cout<<"Answer is: "<<ans;
+}
