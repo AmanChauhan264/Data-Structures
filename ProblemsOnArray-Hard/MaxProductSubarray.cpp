68c4caed19be00f007d38c323a885d0e03cfd140
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-17 07:17:59 +0530

    push optimal sol for maximum product subarray

--- ProblemsOnArray-Hard/MaxProductSubarray.cpp
+++ ProblemsOnArray-Hard/MaxProductSubarray.cpp
@@ -1,17 +1,52 @@
+//     BETTER THAN BRUTE
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int MaxProduct(vector<int> &nums){
+//     int n = nums.size();
+//     int maxi = INT_MIN;
+//     for(int i = 0; i<n; i++){
+//         int pr = 1;
+//         for(int j = i; j<n; j++){ 
+//             pr = pr * nums[j];
+//             maxi = max(maxi, pr); 
+//             }
+//     }
+//     return maxi;
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter size of your array: ";
+//     cin>>n;
+//     vector<int> arr(n);
+//     cout<<"Enter elements: ";
+//     for(int i = 0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     int ans = MaxProduct(arr);
+//     cout<<"Answer is: "<<ans;
+// }
+
+//   OPTIMAL SOLUTION 
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int MaxProduct(vector<int> &nums){
+    int pre = 1, suff = 1;
+    int ans = INT_MIN;
     int n = nums.size();
-    int maxi = INT_MIN;
     for(int i = 0; i<n; i++){
-        int pr = 1;
-        for(int j = i; j<n; j++){ 
-            pr = pr * nums[j];
-            maxi = max(maxi, pr); 
-            }
+        if(pre ==0) pre = 1;
+        if(suff ==0) suff = 1;
+
+        pre = pre * nums[i];
+        suff = suff * nums[n-i-1];
+        ans = max(ans, max(pre, suff));
     }
-    return maxi;
+    return ans;
 }
 
 int main(){
