7c4588d4a25c71687a42d62ab7997f0a29d16b91
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-18 07:03:04 +0530

    push lower bound code

---
+++ Binary_Search/LowerBound.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lower_bound(vector<int> &arr, int k){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int ans = n;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] >= k){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n, k;
+    cout<<"Enter the sixe of the array: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter elements: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter your value: ";
+    cin>>k;
+    int ans = lower_bound(arr, k);
+    cout<<"Lower bound is: "<<ans;
+}
