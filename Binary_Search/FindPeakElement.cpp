45f45a052e0c1a37054fb58bb4e55ed36b4d381b
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-08-21 22:29:33 +0530

    push peak element code

---
+++ Binary_Search/FindPeakElement.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int PeakElement(vector<int> &arr){
+   int n = arr.size();
+   if(arr[0] > arr[1]) return arr[0];
+   if(arr[n-1] > arr[n-2]) return arr[n-1];
+   int low = 1, high = n-2;
+   while(low <= high){
+    int mid = (low + high) / 2;
+    if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) {
+        return arr[mid];
+    }
+    else if(arr[mid] > arr[mid-1]){
+         low = mid + 1;
+        }
+    else {
+        high = mid - 1;
+    }
+   }
+   return -1;
+}
+   
+int main(){
+    int n;
+    cout<<"Enter number of elements in the array: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter array with altleast 1 peak element: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    int ans = PeakElement(arr);
+    cout<<"Peak element is: "<<ans;
+    return 0;
+}
