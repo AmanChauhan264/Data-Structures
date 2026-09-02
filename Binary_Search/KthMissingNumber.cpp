d4e9e7a6e22bc485ab9d1518235c9bc61155bd27
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-09-02 21:27:55 +0530

    push optimal sol for kth missing number

--- Binary_Search/KthMissingNumber.cpp
+++ Binary_Search/KthMissingNumber.cpp
@@ -1,13 +1,48 @@
+// Brute force solution
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int KthNumber(vector<int> &arr, int k){
+//     int n = arr.size();
+//     for(int i = 0; i<n; i++){
+//         if(arr[i] <= k) k++;
+//         else break;
+//     }
+//     return k;
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter size of the array: ";
+//     cin>>n;
+//     vector<int> arr(n);
+//     for(int i = 0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     int k;
+//     cout<<"Enter the Kth number: ";
+//     cin>>k;
+//     int ans = KthNumber(arr, k);
+//     cout<<"Answer is: "<<ans;
+//     return 0;
+// }
+
+// Binary search sol
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int KthNumber(vector<int> &arr, int k){
-    int n = arr.size();
-    for(int i = 0; i<n; i++){
-        if(arr[i] <= k) k++;
-        else break;
-    }
-    return k;
+   int n = arr.size();
+   int low = 0, high = n-1;
+   while(low <= high){
+    int mid = (low + high)/2;
+    int missing = arr[mid] - (mid+1);
+    if(missing < k) low = mid+1;
+    else high = mid-1;
+   }
+   return high + 1 + k;
 }
 
 int main(){
